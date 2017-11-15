0211696a0a04f5927da87c94083e01d84403c337
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-15 16:10:59 -0700

    create goal component complete

--- src/goalCreateScreen/components/GoalCreateOutline.js
+++ src/goalCreateScreen/components/GoalCreateOutline.js
@@ -6,6 +6,7 @@ import {
     View,
     ScrollView,
     Button,
+    Picker,
     StyleSheet} from 'react-native';
 
 export default class GoalCreate extends Component {
@@ -71,6 +72,58 @@ export default class GoalCreate extends Component {
                     <View>
                         <Text style={styles.text}>Team Size Limit</Text>
                     </View>
+                    <Picker style={styles.picker}>
+                        <Picker.Item
+                            color='white'
+                            label='1'
+                            value='1'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='2'
+                            value='2'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='3'
+                            value='3'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='4'
+                            value='4'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='5'
+                            value='5'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='6'
+                            value='6'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='7'
+                            value='7'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='8'
+                            value='8'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='9'
+                            value='9'
+                        />
+                        <Picker.Item
+                            color='white'
+                            label='10'
+                            value='10'
+                        />
+                    </Picker>
                 </View>
             </ScrollView>
         );
@@ -112,6 +165,12 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         color:'#b21726',
         marginTop: 30
+    },
+    picker: {
+        backgroundColor: 'black',
+        width: '100%',
+        margin:0,
+        padding: 0
     }
 });
 
