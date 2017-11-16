e918794b942723996fe7836778ec319279bcdcc1
Ismael Perez <ismaelperezemail@gmail.com>
2017-11-16 12:23:28 -0700

    User profile outline complete

--- src/userProfileScreen/components/UserProfileOutline.js
+++ src/userProfileScreen/components/UserProfileOutline.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 
 import * as Progress from 'react-native-progress';
+
 export default class UserProfile extends Component {
     state = {
         curTime: null
@@ -26,26 +27,55 @@ export default class UserProfile extends Component {
 
         return (
             <ScrollView style={styles.scrollCont}>
-                <View style={styles.titleStyle}>
+                <View style={styles.titleCont}>
                     <Text style={styles.title}>Your Progress</Text>
                 </View>
-                    <View style={styles.nameCont}>
-                        <Text style={styles.nameText}>Name Here</Text>
+                <View style={styles.nameCont}>
+                    <Text style={styles.nameText}>Name Here</Text>
+                </View>
+                <View style={styles.progressBarCont}>
+                    <Progress.Bar
+                        progress={.5}
+                        height={20}
+                        animated={true}
+                        color={'#b21726'}
+                        borderWidth={3}/>
+                    <View style={styles.daysLeftCont}>
+                        <Text style={styles.daysLeftText}>Days Left: 7</Text>
                     </View>
-                {/*<Progress.Circle size={30} indeterminate={true} />*/}
-                    <View style={styles.btnCont}>
-                        <View style={styles.dateCont}>
-                            <Text style={styles.date}>{this.state.curTime}</Text>
-                        </View>
-                        <TouchableOpacity
-                            style={styles.signUpButton}
-                            onPress={printThing = () => {
-                                alert("This does nothing")
-                            }}
-                            color='silver'>
-                            <Text style={styles.signUpText}>Complete Workout</Text>
-                        </TouchableOpacity>
-                    </View>
+                </View>
+                <View style={styles.dateCont}>
+                    <Text style={styles.date}>{this.state.curTime}</Text>
+                </View>
+                <View style={styles.btnCont}>
+                    <TouchableOpacity
+                        style={styles.completeWorkoutButton}
+                        onPress={printThing = () => {
+                            alert("This does nothing")
+                        }}
+                        color='silver'>
+                        <Text style={styles.signUpText}>Complete Workout</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.teamPageCreatGoalBtnCont}>
+                    <TouchableOpacity
+                        style={styles.teamPageButton}
+                        onPress={printThing = () => {
+                            alert("This does nothing")
+                        }}
+                        color='silver'>
+                        <Text style={styles.signUpText}>Team Page</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.teamPageButton}
+                        onPress={printThing = () => {
+                            alert("This does nothing")
+                        }}
+                        color='silver'>
+                        <Text style={styles.signUpText}>Create Goal</Text>
+                    </TouchableOpacity>
+                </View>
+
 
             </ScrollView>
         );
@@ -54,27 +84,30 @@ export default class UserProfile extends Component {
 
 const styles = StyleSheet.create({
     scrollCont: {
-        flex: 1
+        flex: 1,
     },
-    titleStyle: {
+    titleCont: {
         marginTop: '13%',
-        // justifyContent: 'center',
         alignItems: 'center',
         elevation: 3,
-        position: 'relative',
-        flex:1
+        flex: 1
+    },
+    title: {
+        fontSize: 40,
+        textAlign: 'center',
+        color: '#b21726',
+        width: '40%',
     },
     btnCont: {
-        height: '20%',
+        height: '8%',
         width: '100%',
-        marginTop: '13%',
+        marginTop: '5%',
         justifyContent: 'center',
         alignItems: 'center',
         elevation: 3,
-        position: 'relative'
     },
     nameCont: {
-        marginTop: '15%',
+        marginTop: '13%',
         alignItems: 'center',
         flex: 1
     },
@@ -83,43 +116,60 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: '500',
     },
+    daysLeftCont: {
+        marginTop: 20,
+        alignItems: 'center',
+        flex: 1
+    },
+    daysLeftText: {
+        color: 'silver',
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    progressBarCont: {
+        alignItems: 'center',
+        marginTop: 10
+    },
     text: {
         color: 'silver'
     },
     dateCont: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: '10%'
+        marginTop: '13%',
+        flex:1
     },
     date: {
         fontSize: 20,
-        fontWeight: '500',
-        height: 50,
-        width: '100%',
-        color: '#b21726'
-    },
-    title: {
-        fontSize: 40,
-        // height: '30%',
-        textAlign: 'center',
+        fontWeight: '600',
+        height: 20,
         color: '#b21726',
-        width: '40%',
     },
-    signUpButton: {
+    completeWorkoutButton: {
         backgroundColor: '#222222',
-        width: '50%',
+        width: '80%',
         flex: 1,
         borderWidth: 1,
         borderRadius: 20,
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 10
+        marginTop: 5
     },
     signUpText: {
         color: 'silver',
         fontWeight: '600',
         fontSize: 16,
+    },
+    teamPageCreatGoalBtnCont: {
+        height: '5%',
+        width: '100%',
+        marginTop: '28%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 3,
+    },
+    teamPageButton: {
+        marginBottom: 15
     }
 });
 
